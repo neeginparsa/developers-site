ae8ba9f591bf72b401a6144d96a1666473eadd21
neeginparsa <neeginparsa2001@gmail.com>
2022-02-11 13:44:51 +0330

    templates

--- developersearchsite/settings.py
+++ developersearchsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'projects',
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -56,8 +59,9 @@ ROOT_URLCONF = 'developersearchsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
+
+def projects(request):
+    context = {'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
+
+
+def project(request, pk):
+    project_obj = None
+
+    for project in projectsList:
+        if project['id'] == pk:
+            project_obj = project
+
+    context = {'project': project_obj}
+    return render(request, 'projects/single_project.html', context)
