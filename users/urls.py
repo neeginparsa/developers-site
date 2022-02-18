03c113d4a6cbbd95302550410bd9f6a6533a4274
neeginparsa <neeginparsa2001@gmail.com>
2022-02-18 04:11:06 +0330

    authentication

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns =[
     path('login/', views.LoginUser, name='login'),
     path('logout/', views.LogoutUser, name='logout'),
+    path('register/', views.RegisterUser, name='register'),
 
     path('', views.Profiles, name='profiles'),
     path('profile/<str:pk>', views.userProfile, name='user-profile'),
--- users/views.py
+++ users/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 from django.contrib.auth.models import User
-
 from .models import Profile
 
 
 def LoginUser(request):
+
+    page = 'login'
+
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -29,13 +32,32 @@ def LoginUser(request):
         else:
             messages.error(request, 'username or password incorect')
 
-    return render(request, 'users/login-register.html')
+    return render(request, 'users/login-register.html', )
 
 def LogoutUser(request):
     logout(request)
     messages.error(request, 'user logged out')
     return redirect('login')
 
+def RegisterUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            #2 khat payiin baraye tabdil matn karbar be lowercase va sepas save
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'account was created')
+
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.success(request, 'an error has accurred during registration')
+    context = {'page': page, 'form':form}
+    return render(request, 'users/login-register.html', context)
 
 def Profiles(request):
     profiles = Profile.objects.all()
