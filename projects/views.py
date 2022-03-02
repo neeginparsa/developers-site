ce4038faf20cd75f9cc6b9d66edae08ad29c23a0
neeginparsa <neeginparsa2001@gmail.com>
2022-03-03 02:40:52 +0330

    projects search box

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, redirect
-from .models import Project
+from .models import Project, Tag
 from .forms import ProjectForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 import os
 from django.http import HttpResponse
 from django.http import Http404
 from django.conf import settings
 
 def projects(request):
-    projects = Project.objects.all()
-    context = {'projects': projects}
+    search_query = ''
+
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+
+    tags = Tag.objects.filter(name__icontains=search_query)
+    projects = Project.objects.distinct().filter(Q(title__icontains=search_query) |
+                                      Q(description__icontains=search_query) |
+                                      Q(owner__name__icontains=search_query) |
+                                      Q(tags__in=tags))
+    context = {'projects': projects, 'search_query': search_query}
     return render(request, 'projects/projects.html', context)
 
 
