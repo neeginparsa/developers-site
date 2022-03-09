9d0c763895dd5051456e4969b4508ec228d7bbae
neeginparsa <neeginparsa2001@gmail.com>
2022-03-09 04:26:58 +0330

    send message & inbox

--- users/views.py
+++ users/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from django.contrib.auth.models import User
-from .models import Profile, Skill
+from .models import Profile, Skill, Message
 from django.db.models import Q
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
@@ -31,7 +31,7 @@ def LoginUser(request):
 
         if user is not None:
             login(request, user)
-            return redirect(request.GET['next']if 'next' in request.GET else 'account')
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
         else:
             messages.error(request, 'username or password incorect')
 
@@ -184,3 +184,56 @@ def deleteSkill(request, pk):
 
     context = {'object': skill}
     return render(request, 'delete_obj.html', context)
+
+
+@login_required(login_url='login')
+def inbox(request):
+    profile = request.user.profile
+    #get messages
+    messageRequest = profile.messages.all()
+    unreadCount = messageRequest.filter(is_read= False).count()
+
+    context = {'messageRequest' : messageRequest, 'unreadCount': unreadCount}
+
+    return render(request, 'users/inbox.html', context)
+
+
+@login_required(login_url='login')
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+
+    if message.is_read == False:
+        message.is_read = True
+        message.save()
+
+    context = {'message': message}
+    return render(request, 'users/message.html', context)
+
+
+def createmessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+    #for users that non login or login
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+            messages.success(request, 'your message was successfully sent!')
+            return redirect('user-profile', pk=recipient.id)
+
+    context = {'recipient': recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)
